3999923ca0b73c72d3f182691d1a858210bdd113
ShahinurAlamBhuiyan <shahinbhuiyan19sep@gmail.com>
2021-06-21 00:16:43 +0600

    server foldering

---
+++ index.js
@@ -0,0 +1,38 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import dotenv from 'dotenv';
+
+import postRoutes from './routes/posts.js';
+
+
+const app = express();
+dotenv.config();
+
+
+app.use(bodyParser.json({limit: '30mb', extended: true}));
+app.use(bodyParser.urlencoded({limit: '30mb', extended: true}));
+app.use(cors());
+
+
+app.use('/posts', postRoutes);
+
+app.get('/', (req, res) => {
+    res.send('Hello, welcome to memories projects servers')
+})
+
+
+const CONNECTION_URL =`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.mxfb0.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+const PORT = process.env.PORT || 5000;
+
+mongoose
+  .connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() =>
+    app.listen(PORT, () =>
+      console.log(`Server Running on Port: http://localhost:${PORT}`)
+    )
+  )
+  .catch((error) => `${error} did not connect`);
+
+mongoose.set("useFindAndModify", false);
