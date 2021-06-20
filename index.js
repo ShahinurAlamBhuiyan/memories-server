eae79986c8994cb10aa4645d78631ec2a6d2d04f
ShahinurAlamBhuiyan <shahinbhuiyan19sep@gmail.com>
2021-06-21 01:07:09 +0600

    server foldering again

--- index.js
+++ index.js
@@ -1,18 +1,17 @@
 import express from 'express';
-import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
 import dotenv from 'dotenv';
-
 import postRoutes from './routes/posts.js';
+
 
 
 const app = express();
 dotenv.config();
 
 
-app.use(bodyParser.json({limit: '30mb', extended: true}));
-app.use(bodyParser.urlencoded({limit: '30mb', extended: true}));
+app.use(express.json({limit: '30mb', extended: true}));
+app.use(express.urlencoded({limit: '30mb', extended: true}));
 app.use(cors());
 
 
