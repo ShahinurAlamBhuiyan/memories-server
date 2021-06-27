485b5490715e152b265f9832114175a61ef1fa22
ShahinurAlamBhuiyan <shahinbhuiyan19sep@gmail.com>
2021-06-28 01:23:23 +0600

    add authentication setup and jwt

--- controllers/posts.js
+++ controllers/posts.js
@@ -18,7 +18,7 @@ export const getPosts = async(req, res) =>{
 export const createPost = async(req, res) =>{
     const post = req.body;
 
-    const newPost = new PostMessage(post);
+    const newPost = new PostMessage({...post, creator: req.userId, createdAt: new Date().toISOString()});
    
     try {
         await newPost.save();
@@ -56,10 +56,21 @@ export const deletePost = async(req, res) =>{
 export const likePost = async(req, res) =>{
     const { id :_id } = req.params;
 
+    if(!req.userId) return res.json({ message: 'Unauthenticated' })
+
     if(!mongoose.Types.ObjectId.isValid(_id)) return res.status(404).send("No post with that id");
 
     const post = await PostMessage.findById(_id);
-    const updatedPost = await PostMessage.findByIdAndUpdate(_id, { likeCount : post.likeCount+ 1}, {new: true});
+
+    const index = post.likes.findIndex( (id) => id === String(req.userId));
+
+    if(index === -1){
+        post.likes.push(req.userId);
+    } else {
+        post.likes = post.likes.filter((id) => id !== String(req.userId));
+    }
+
+    const updatedPost = await PostMessage.findByIdAndUpdate(_id, post, {new: true});
 
 
     res.json(updatedPost);
--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import mongoose from 'mongoose';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import postRoutes from './routes/posts.js';
+import userRoutes from './routes/users.js';
 
 
 
@@ -16,6 +17,7 @@ app.use(cors());
 
 
 app.use('/posts', postRoutes);
+app.use('/user', userRoutes);
 
 app.get('/', (req, res) => {
     res.send('Hello, welcome to memories server')
