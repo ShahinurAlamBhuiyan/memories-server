144a50c50ed037e95f2f40047f07b0adbc10ff62
ShahinurAlamBhuiyan <shahinbhuiyan19sep@gmail.com>
2021-07-12 23:24:55 +0600

    fixing some bug.Like,show most recent post first and so on

--- controllers/posts.js
+++ controllers/posts.js
@@ -10,7 +10,7 @@ export const getPosts = async (req, res) => {
         const startIndex = (Number(page) - 1) * LIMIT; // get the starting index of every page
         const total = await PostMessage.countDocuments({});
 
-        const posts = await PostMessage.find().sort({ _id: 1 }).limit(LIMIT).skip(startIndex);
+        const posts = await PostMessage.find().sort({ _id: -1 }).limit(LIMIT).skip(startIndex);
 
         res.status(200).json({ data: posts, currentPage: Number(page), numberOfPages: Math.ceil(total / LIMIT)});
 
