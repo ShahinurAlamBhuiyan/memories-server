4079c14ae5af1a8a1e8653c5877c0977baef963b
ShahinurAlamBhuiyan <shahinbhuiyan19sep@gmail.com>
2024-08-02 19:02:22 +0600

    bug fixing

--- index.js
+++ index.js
@@ -11,20 +11,26 @@ const app = express();
 dotenv.config();
 
 
-app.use(express.json({limit: '30mb', extended: true}));
-app.use(express.urlencoded({limit: '30mb', extended: true}));
-app.use(cors());
+app.use(express.json({ limit: '30mb', extended: true }));
+app.use(express.urlencoded({ limit: '30mb', extended: true }));
+app.use(cors({
+  origin: ["http://localhost:3000"],
+  methods: ["POST", "GET", "PATCH", "DELETE"],
+  credentials: true
+}));
 
 
 app.use('/posts', postRoutes);
 app.use('/user', userRoutes);
 
 app.get('/', (req, res) => {
-    res.send('Hello, welcome to memories server side')
+  res.send('Hello, welcome to memories server side')
 })
 
 
-const CONNECTION_URL =`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.mxfb0.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+// const CONNECTION_URL =`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.qb2bftr.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+const CONNECTION_URL = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.qb2bftr.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority&appName=${process.env.DB_NAME}`
+
 
 const PORT = process.env.PORT || 5000;
 
