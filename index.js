498bd32158729e58a57b4ed349d538fb9c749ec7
ShahinurAlamBhuiyan <shahinbhuiyan19sep@gmail.com>
2024-08-02 19:45:13 +0600

    bugs

--- index.js
+++ index.js
@@ -5,12 +5,10 @@ import dotenv from 'dotenv';
 import postRoutes from './routes/posts.js';
 import userRoutes from './routes/users.js';
 
-
-
 const app = express();
 dotenv.config();
 
-
+// Middleware
 app.use(express.json({ limit: '30mb', extended: true }));
 app.use(express.urlencoded({ limit: '30mb', extended: true }));
 app.use(cors({
@@ -19,28 +17,27 @@ app.use(cors({
   credentials: true
 }));
 
+app.options('*', cors()); // Handle preflight requests
 
+// Routes
 app.use('/posts', postRoutes);
 app.use('/user', userRoutes);
 
 app.get('/', (req, res) => {
-  res.send('Hello, welcome to memories server side')
-})
+  res.send('Hello, welcome to memories server side');
+});
 
-
-// const CONNECTION_URL =`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.qb2bftr.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
-const CONNECTION_URL = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.qb2bftr.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority&appName=${process.env.DB_NAME}`
-
-
+// MongoDB Connection
+const CONNECTION_URL = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.qb2bftr.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority&appName=${process.env.DB_NAME}`;
 const PORT = process.env.PORT || 5000;
 
 mongoose
   .connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() =>
+  .then(() => {
     app.listen(PORT, () =>
-      console.log(`Server Run on Port: http://localhost:${PORT}`)
-    )
-  )
-  .catch((error) => `${error} did not connect`);
+      console.log(`Server running on Port: http://localhost:${PORT}`)
+    );
+  })
+  .catch((error) => console.error('MongoDB connection error:', error));
 
 mongoose.set("useFindAndModify", false);
